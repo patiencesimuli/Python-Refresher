bc511c3675e097f3672d3c76461e5f83a38a0678
Patience Simuli <101804617+patiencesimuli@users.noreply.github.com>
2023-10-03 00:40:56 +0300

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -1,7 +1,17 @@
 secret_word = "intelligence"
 user_guess = " "
+guess_count = 0
+guess_limit = 3
+out_of_guesses = False
 
 while user_guess != secret_word:
-    user_guess = input ("Enter your guess: ")
+    if (guess_count < guess_limit):
+        user_guess = input ("Enter your guess: ")
+        guess_count ++
+    else:
+        out_of_guesses = True
 
-print ("You win!")
+if out_of_guesses:
+    print ("You are out of guesses, GAME LOST")
+else:
+    print ("You win!")
